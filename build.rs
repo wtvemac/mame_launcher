0387c550e92dbd8867154bcc9881d489922798d3
wtvemac <wtvemac@proton.me>
2024-12-04 20:05:23 -0500

    Application icon

--- build.rs
+++ build.rs
@@ -1,4 +1,13 @@
 fn main() {
+	#[cfg(target_os = "windows")]
+	winresource::WindowsResource::new()
+		.set("ProductName", "WebTV MAME Launcher")
+		.set("FileDescription", "WebTV MAME Launcher")
+		.set("LegalCopyright", "Unlicensed")
+		.set_icon("ui/images/icon.ico")
+		.compile()
+			.expect("Failed to run the Windows resource compiler (rc.exe)");
+
 	let config= slint_build::CompilerConfiguration::new().with_style("fluent-dark".into());
 
 	slint_build::compile_with_config("ui/mainwindow.slint", config).unwrap();
