c85238ee4f76f90ced4ea4875f34404be8303b57
wtvemac <wtvemac@proton.me>
2025-06-07 02:21:26 -0400

    Improve ROMIO implementation
    Simplifying code and change to support new BuildIO features

--- src/wtv/buildio/romio.rs
+++ src/wtv/buildio/romio.rs
@@ -1,13 +1,13 @@
-use super::BuildIO;
+use super::{BuildIO, BuildIODataCollation};
 use std::fs::File;
 use std::io::{Read, Write, Seek, SeekFrom};
 
 #[allow(dead_code)]
 pub struct ROMIO {
-	stripped: bool,
-	rom_size: u32,
 	file_path: String,
-	f0: Option<File>,
+	collation: BuildIODataCollation,
+	size: u64,
+	f0: File,
 	f1: Option<File>
 }
 
@@ -16,49 +16,63 @@ impl BuildIO for ROMIO {
 		Ok(self.file_path.clone())
 	}
 
-	fn open(file_path: String, stripped: Option<bool>, rom_size: Option<u32>) -> Result<Option<ROMIO>, Box<dyn std::error::Error>> {
-		let mut wtvbuild_io = ROMIO {
-			stripped: stripped.unwrap_or(false),
-			rom_size: rom_size.unwrap_or(0x000000),
-			file_path: file_path.clone(),
-			f0: None,
-			f1: None
-		};
+	fn open(file_path: String, collation: Option<BuildIODataCollation>) -> Result<Box<dyn BuildIO>, Box<dyn std::error::Error>> {
+		let mut io;
 
-		if wtvbuild_io.stripped {
-			wtvbuild_io.f0 = Some(File::open(file_path.clone() + "0")?);
-			wtvbuild_io.f1 = Some(File::open(file_path.clone() + "1")?);
+		if collation.unwrap_or(BuildIODataCollation::Raw) == BuildIODataCollation::StrippedROMs {
+			io = ROMIO {
+				file_path: file_path.clone(),
+				collation: collation.unwrap_or(BuildIODataCollation::Raw),
+				size: 0,
+				f0: File::open(file_path.clone() + "0")?,
+				f1: Some(File::open(file_path.clone() + "1")?)
+			};
+
+			io.size = io.f0.metadata().unwrap().len() * 2;
 		} else {
-			wtvbuild_io.f0 = Some(File::open(file_path.clone())?);
+			io = ROMIO {
+				file_path: file_path.clone(),
+				collation: collation.unwrap_or(BuildIODataCollation::Raw),
+				size: 0,
+				f0: File::open(file_path.clone())?,
+				f1: None
+			};
+
+			io.size = io.f0.metadata().unwrap().len();
 		}
 
-		Ok(Some(wtvbuild_io))
+		Ok(Box::new(io))
 	}
 
-	fn create(file_path: String, stripped: Option<bool>, rom_size: Option<u32>) -> Result<Option<ROMIO>, Box<dyn std::error::Error>> {
-		let mut wtvbuild_io = ROMIO {
-			stripped: stripped.unwrap_or(false),
-			rom_size: rom_size.unwrap_or(0x000000),
-			file_path: file_path.clone(),
-			f0: None,
-			f1: None
-		};
+	fn create(file_path: String, collation: Option<BuildIODataCollation>, size: u64) -> Result<Box<dyn BuildIO>, Box<dyn std::error::Error>> {
+		let io;
 
-		if wtvbuild_io.stripped {
-			wtvbuild_io.f0 = Some(File::create(file_path.clone() + "0")?);
-			wtvbuild_io.f1 = Some(File::create(file_path.clone() + "1")?);
+		if collation.unwrap_or(BuildIODataCollation::Raw) == BuildIODataCollation::StrippedROMs {
+			io = ROMIO {
+				file_path: file_path.clone(),
+				collation: collation.unwrap_or(BuildIODataCollation::Raw),
+				size: size,
+				f0: File::create(file_path.clone() + "0")?,
+				f1: Some(File::create(file_path.clone() + "1")?)
+			};
 		} else {
-			wtvbuild_io.f0 = Some(File::create(file_path.clone())?);
+			io = ROMIO {
+				file_path: file_path.clone(),
+				collation: collation.unwrap_or(BuildIODataCollation::Raw),
+				size: size,
+				f0: File::create(file_path.clone())?,
+				f1: None
+			};
 		}
 
-		Ok(Some(wtvbuild_io))
+		Ok(Box::new(io))
 	}
 
 	fn seek(&mut self, pos: u64) -> Result<u64, Box<dyn std::error::Error>>  {
-		if self.stripped {
+		if self.collation == BuildIODataCollation::StrippedROMs {
 			//let wanted_pos: i64 = pos.into();
 
-			let f0_seek = self.f0.as_ref().unwrap().seek(SeekFrom::Start(pos / 2))?;
+			let f0_seek = self.f0.seek(SeekFrom::Start(pos / 2))?;
 			let f1_seek = self.f1.as_ref().unwrap().seek(SeekFrom::Start(pos / 2))?;
 
 			if f0_seek == f1_seek {
@@ -67,21 +81,21 @@ impl BuildIO for ROMIO {
 				Ok(0)
 			}
 		} else {
-			Ok(self.f0.as_ref().unwrap().seek(SeekFrom::Start(pos))?)
+			Ok(self.f0.seek(SeekFrom::Start(pos))?)
 		}
 	}
 
 	fn read(&mut self, buf: &mut [u8]) -> Result<usize, Box<dyn std::error::Error>> {
-		if self.stripped {
+		if self.collation == BuildIODataCollation::StrippedROMs {
 			if buf.len() < 0x4 {
-				panic!("Buffer needs to be greater than 4 bytes.");
-			} else if (buf.len() % 2) == 1 {
-				panic!("Buffer needs to be a multiple of 2.");
+				Err("Buffer needs to be greater than 4 bytes.".into())
+			} else if (buf.len() & 1) == 1 {
+				Err("Buffer needs to be a multiple of 2.".into())
 			} else {
-				let mut rsize: usize = 0x0;
+					let mut rsize: usize = 0x0;
 
 				for index in (0..buf.len()).step_by(4) {
-					rsize += self.f0.as_ref().unwrap().read(&mut buf[(index + 0)..(index + 2)])?;
+					rsize += self.f0.read(&mut buf[(index + 0)..(index + 2)])?;
 
 					// Stop reading if the buffer is a miltiple of 2 but not a multiple of 4. For example, like reading into a 62 byte buffer.
 					if (index + 4) <= buf.len() {
@@ -92,24 +106,24 @@ impl BuildIO for ROMIO {
 				Ok(rsize)
 			}
 		} else {
-			Ok(self.f0.as_ref().unwrap().read(buf)?)
+			Ok(self.f0.read(buf)?)
 		}
 	}
 
 	fn write(&mut self, buf: &mut [u8]) -> Result<usize, Box<dyn std::error::Error>> {
-		if self.stripped {
+		if self.collation == BuildIODataCollation::StrippedROMs {
 			if buf.len() < 0x4 {
-				panic!("Buffer needs to be greater than 4 bytes.");
-			} else if (buf.len() % 2) == 1 {
-				panic!("Buffer needs to be a multiple of 2.");
+				Err("Buffer needs to be greater than 4 bytes.".into())
+			} else if (buf.len() & 1) == 1 {
+				Err("Buffer needs to be a multiple of 2.".into())
 			} else {
-				let mut rsize: usize = 0x0;
+					let mut rsize: usize = 0x0;
 
 				for index in (0..buf.len()).step_by(4) {
 					unsafe {
 						let buf1: [u8; 2] = [*buf.get_unchecked_mut(index + 0), *buf.get_unchecked_mut(index + 1)];
 
-						rsize += self.f0.as_ref().unwrap().write(&buf1)?;
+						rsize += self.f0.write(&buf1)?;
 
 						// Write null padding if the buffer is a miltiple of 2 but not a multiple of 4. For example, like writing from a 62 byte buffer.
 						if (index + 4) <= buf.len() {
@@ -126,15 +140,19 @@ impl BuildIO for ROMIO {
 				Ok(rsize)
 			}
 		} else {
-			Ok(self.f0.as_ref().unwrap().write(buf)?)
+			Ok(self.f0.write(buf)?)
 		}
 	}
 
 	fn len(&mut self) -> Result<u64, Box<dyn std::error::Error>> {
-		if self.stripped {
-			Ok(self.f0.as_ref().unwrap().metadata().unwrap().len() * 2)
+		if self.collation == BuildIODataCollation::StrippedROMs {
+			Ok(self.size * 2)
 		} else {
-			Ok(self.f0.as_ref().unwrap().metadata().unwrap().len())
+			Ok(self.size)
 		}
+	}
+
+	fn collation(&mut self) -> Result<BuildIODataCollation, Box<dyn std::error::Error>> {
+		Ok(self.collation)
 	}
 }
