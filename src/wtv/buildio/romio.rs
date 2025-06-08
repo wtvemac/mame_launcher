294c1c49efe64fbccb7ff78472efb76a507d16a6
wtvemac <wtvemac@proton.me>
2025-06-08 06:36:08 -0400

    Clamp reading and writing to ROMIO size

--- src/wtv/buildio/romio.rs
+++ src/wtv/buildio/romio.rs
@@ -90,6 +90,8 @@ impl BuildIO for ROMIO {
 	}
 
 	fn read(&mut self, buf: &mut [u8]) -> Result<usize, Box<dyn std::error::Error>> {
+		let corrected_size = buf.len().min((self.len().unwrap_or(0) - self.stream_position().unwrap_or(0)) as usize);
+
 		if self.collation == BuildIODataCollation::StrippedROMs {
 			if buf.len() < 0x4 {
 				Err("Buffer needs to be greater than 4 bytes.".into())
@@ -99,11 +101,11 @@ impl BuildIO for ROMIO {
 				let mut rsize: usize = 0x0;
 
 				if self.collation == BuildIODataCollation::StrippedROMs {
-					for index in (0..buf.len()).step_by(4) {
+					for index in (0..corrected_size).step_by(4) {
 						rsize += self.f0.read(&mut buf[(index + 0)..(index + 2)])?;
 
 						// Stop reading if the buffer is a miltiple of 2 but not a multiple of 4. For example, like reading into a 62 byte buffer.
-						if (index + 4) <= buf.len() {
+						if (index + 4) <= corrected_size {
 							rsize += self.f1.as_ref().unwrap().read(&mut buf[(index + 2)..(index + 4)])?;
 						}
 					}
@@ -114,11 +116,13 @@ impl BuildIO for ROMIO {
 				Ok(rsize)
 			}
 		} else {
-			Ok(self.f0.read(buf)?)
+			Ok(self.f0.read(&mut buf[0..corrected_size])?)
 		}
 	}
 
 	fn write(&mut self, buf: &mut [u8]) -> Result<usize, Box<dyn std::error::Error>> {
+		let corrected_size = buf.len().min((self.len().unwrap_or(0) - self.stream_position().unwrap_or(0)) as usize);
+
 		if self.collation == BuildIODataCollation::StrippedROMs {
 			if buf.len() < 0x4 {
 				Err("Buffer needs to be greater than 4 bytes.".into())
@@ -127,31 +131,27 @@ impl BuildIO for ROMIO {
 			} else {
 				let mut rsize: usize = 0x0;
 
-				if self.collation == BuildIODataCollation::StrippedROMs {
-					let mut buf0= vec![0x00 as u8; buf.len()/2];
-					let mut buf1 = vec![0x00 as u8; buf.len()/2];
+				let mut buf0 = vec![0x00 as u8; corrected_size/2];
+				let mut buf1 = vec![0x00 as u8; corrected_size/2];
 
-					let mut stripped_bufindex = 0;
-					for whole_bufindex in (0..buf.len()).step_by(4) {
-						buf0[stripped_bufindex + 0] = buf[whole_bufindex + 0];
-						buf0[stripped_bufindex + 1] = buf[whole_bufindex + 1];
+				let mut stripped_bufindex = 0;
+				for whole_bufindex in (0..corrected_size).step_by(4) {
+					buf0[stripped_bufindex + 0] = buf[whole_bufindex + 0];
+					buf0[stripped_bufindex + 1] = buf[whole_bufindex + 1];
 
-						buf1[stripped_bufindex + 0] = buf[whole_bufindex + 2];
-						buf1[stripped_bufindex + 1] = buf[whole_bufindex + 3];
-						
-						stripped_bufindex += 2;
-					}
-
-					rsize += self.f0.write(&buf0)?;
-					rsize += self.f1.as_ref().unwrap().write(&buf1)?;
-				} else {
-					rsize += self.f0.write(&buf)?;
+					buf1[stripped_bufindex + 0] = buf[whole_bufindex + 2];
+					buf1[stripped_bufindex + 1] = buf[whole_bufindex + 3];
+					
+					stripped_bufindex += 2;
 				}
+
+				rsize += self.f0.write(&buf0)?;
+				rsize += self.f1.as_ref().unwrap().write(&buf1)?;
 
 				Ok(rsize)
 			}
 		} else {
-			Ok(self.f0.write(buf)?)
+			Ok(self.f0.write(&buf[0..corrected_size])?)
 		}
 	}
 
